ceac72c766f14a443a12e69523d37ef81da34a6d
monicaGY <monicagarciayadaicela@gmail.com>
2023-09-04 11:00:50 +0200

    crear categoria

--- src/Controller/DoctrineCategoriaController.php
+++ src/Controller/DoctrineCategoriaController.php
@@ -4,8 +4,11 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
 use App\Entity\Categoria;
 
 class DoctrineCategoriaController extends AbstractController
@@ -38,4 +41,32 @@ class DoctrineCategoriaController extends AbstractController
         }
         return $this->json($datos);
     }
+
+    #[Route('/api/v1/doctrine/categoria', methods:['POST'])]
+    public function crear_categoria(Request $request,SluggerInterface $slugger): JsonResponse
+    {
+
+        $datos = json_decode($request->getContent(), true);
+
+        if(empty($datos['nombre'])){
+            return $this->json([
+                'estado' => 'error',
+                'mensaje' => 'parametros vacíos'
+            ], 200);
+        }
+
+        $categoria = new Categoria();
+
+        $categoria->setNombre($datos['nombre']);
+        $categoria->setSlug($slugger->slug(strtolower($datos['nombre'])));
+
+        $this->em->persist($categoria);
+        $this->em->flush();
+        return $this->json([
+            'estado'=>'ok',
+            'mensaje'=>'Categoría creada con éxito'
+        ], 201);
+    }
+
+    
 }
